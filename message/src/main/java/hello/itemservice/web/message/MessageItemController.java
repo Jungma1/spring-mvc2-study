9c47bb88b10e80d191d20c42a4d74e92a6592667
Jungma1 <mbjun3630@naver.com>
2022-05-07 01:00:33 +0900

    [메시지] 스프링 메시지 소스 설정

--- message/src/main/java/hello/itemservice/web/message/MessageItemController.java
+++ message/src/main/java/hello/itemservice/web/message/MessageItemController.java
@@ -6,7 +6,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -56,6 +60,4 @@ public class MessageItemController {
         itemRepository.update(itemId, item);
         return "redirect:/message/items/{itemId}";
     }
-
 }
-
