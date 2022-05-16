adfb4e90c0d3cde77892eb1c8c0db18f6c14ee8b
Jungma1 <mbjun3630@naver.com>
2022-05-16 16:36:44 +0900

    [빈 검증] 오브젝트 에러 처리

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
+// @ScriptAssert 오브젝트 에러 처리로 권장하지 않음 (자바 코드로 작성하는 것을 권장)
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
