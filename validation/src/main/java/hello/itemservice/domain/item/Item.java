a3384d85d84c2408441e98631cc3b42d88643f57
Jungma1 <mbjun3630@naver.com>
2022-05-19 19:38:53 +0900

    [빈 검증] Form 전송 객체 분리

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,28 +1,15 @@
 package hello.itemservice.domain.item;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 @Data
 // @ScriptAssert 오브젝트 에러 처리로 권장하지 않음 (자바 코드로 작성하는 것을 권장)
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가(groups 사용으로 수정 시에만 검증)
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
